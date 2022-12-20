55c1c66979d28b67b15e83201692afb80486f987
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-20 11:12:58 +0630

    authorization middleware

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -20,27 +20,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  const luckyNumber = Math.floor(Math.random() * 100);
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('No token provided', 401);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  // Check if the token is valid
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const luckyNumber = Math.floor(Math.random() * 100);
-
-    res.status(200).json({
-      message: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data. Your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route', 401);
-  }
+  res.status(200).json({
+    message: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data. Your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
